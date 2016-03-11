2f55c7ce611f4d5929a87fd5bae666c012fc4043
JonathanAndrew <jonandrewstevenson@yahoo.com>
2016-03-10 20:19:30 -0800

    added styling profile and about

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   get 'users/show'
 
-  get 'main/about'
+  get 'about' => 'main#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
